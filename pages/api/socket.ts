78df9bac7d64525abda0369fb24d42a07efdefed
Zevoir Tech <zevoirtech@Zevoirs-MacBook-Pro.local>
2024-04-07 18:58:54 +0530

    fix: socket implementation - 3

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -16,7 +16,10 @@ const socketHandler = (req:any, res:any) => {
     console.log('Socket is already running')
   } else {
     console.log('Socket is initializing')
-    console.log('[res.socket.server]', res.socket.server)
+    console.log('[res.socket.server]', res.socket?.server)
+    if (!res.socket?.server) {
+        return null
+    }
     const io = new Server(res.socket.server)
     console.log('[res.socket.server]', res.socket.server)
     res.socket.server.io = io
